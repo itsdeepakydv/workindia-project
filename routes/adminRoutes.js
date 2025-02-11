4a000a9f7502ce4249335c5d4a8ae437619647e9
itsdeepakydv <114461345+itsdeepakydv@users.noreply.github.com>
2025-02-11 12:02:15 +0530

    Delete routes directory

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminControl');
-const apiKeyMiddleware = require('../middleware/apiKeyMiddleware');
-
-router.post('/addTrain', apiKeyMiddleware.verifyApiKey, adminControl.addTrain);
-router.put('/update-seats/:trainId', apiKeyMiddleware.verifyApiKey, adminControl.updateTrainSeats);
-
-module.exports = router;
